f5e2f9f700866db3995748f70c460b319166b130
Oleg Moroz <oleg.moroz@klaviyo.com>
2025-07-09 14:39:06 -0400

    new logfile for each run, minor updates

--- logging_config.py
+++ logging_config.py
@@ -1,10 +1,16 @@
 import logging
+from datetime import datetime
+
 from rich.logging import RichHandler
 
 
 def set_up_logging() -> None:
-    file_formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
-    file_handler = logging.FileHandler("repro_wrapped.log")
+    file_formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s %(name)s: %(message)s"
+    )
+    file_handler = logging.FileHandler(
+        f"repro_wrapped_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+    )
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(file_formatter)
 
@@ -17,5 +23,5 @@ def set_up_logging() -> None:
         handlers=[
             file_handler,
             rich_handler,
-        ]
+        ],
     )
